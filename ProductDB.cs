b8ce8485d024d33f4420e4f320511e687ff48ff4
garaevo3 <5mptrbf@code.edu.az>
2022-11-21 23:49:06 +0400

    done

---
+++ ProductDB.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometask_4
+{
+    internal class ProductDB
+    {
+        public Product[] products = new Product[5];
+        public int i = 0, itemNum = 0;
+        public bool valid = true;
+
+        public void addToProducts(Product product)
+        {
+            products[i] = product;
+            i++;
+            if (i == products.Length)
+            {
+                valid = false;
+                Console.WriteLine("DATABASE IS FULL NOW!");
+            }
+        }
+        public void ProductsList()
+        {
+            foreach (var item in products)
+            {
+                if (item != null)
+                {
+                    itemNum++;
+                    Console.WriteLine();
+                    Console.WriteLine("Product Number: " + itemNum);
+                    Console.WriteLine("ID: " + item.ID);
+                    Console.WriteLine("Name: " + item.NAME);
+                    Console.WriteLine("Price: " + item.PRICE);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("THERE IS NO PRODUCT");
+                }
+            }
+            itemNum = 0;
+        }
+        public void searchProduct(string name)
+        {
+            foreach (var item in products)
+            {
+                if (name == item.NAME)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("ID: " + item.ID);
+                    Console.WriteLine("Name: " + item.NAME);
+                    Console.WriteLine("Price: " + item.PRICE);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿namespace hometask_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            ProductDB products=new ProductDB();
+            Console.WriteLine();
+            Console.WriteLine("1.ADD PRODUCT");
+            Console.WriteLine("2.SEARCH PRODUCT");
+            Console.WriteLine("3.PRODUCTS LIST");
+            Console.WriteLine("4.LEAVE");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+            while (true)
+            {
+                Console.Write("ENTER VALUE: ");
+                string value = Console.ReadLine();
+                switch (value)
+                {
+                    case "1":
+                        AddProduct();
+                        continue;
+                    case "2":
+                        Console.Write("SEARCH THE PRODUCT BY NAME: ");
+                        products.searchProduct(Console.ReadLine());
+                        continue;
+                    case "3":
+                        Console.WriteLine("PRODUCTS LIST:");
+                        products.ProductsList();
+                        continue;
+                    case "4":
+                        Console.WriteLine("YOU ENDED THE SESSION!");
+                        return;
+                    default:
+                        Console.WriteLine("-PLEASE ENTER A VALID VALUE-");
+                        continue;
+                }
+            }
+            void AddProduct()
+            {
+                if (products.valid)
+                {
+                    i++;
+                    Console.WriteLine("PRODUCT NUMBER: " + i);
+                    Console.WriteLine("ADD PRODUCT ID: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("ADD PRODUCT NAME: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("ADD PRODUCT PRICE: ");
+                    double price = Convert.ToDouble(Console.ReadLine());
+
+                    Product product = new Product
+                    {
+                        ID = id,
+                        NAME = name,
+                        PRICE = price
+                    };
+                    products.addToProducts(product);
+                }
+                else
+                {
+                    Console.WriteLine("DATABASE IS ALREADY FULL!");
+                }
+            }    
+        }
+    }
+}
